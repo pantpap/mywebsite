faa1d54e8dc4101233093e16bd9c1b5c82276c0c
Pantelis Papasavvas <ppapasavvas@antaeustravel.com>
2024-12-30 17:41:44 +0200

    feat(general): route to blog

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', loadComponent: () =>
+      import('@my-website/blog').then(m => m.BlogComponent)
+  },
+];
