829afdceba38a63e7bec193a9c75e22362dc0132
Pantelis Papasavvas <ppapasavvas@antaeustravel.com>
2024-12-30 17:49:57 +0200

    feat(general): add p-card as a demo on blog list

--- libs/feature/src/lib/blog/blog-list/blog-list.component.ts
+++ libs/feature/src/lib/blog/blog-list/blog-list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Card } from 'primeng/card';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'lib-blog-list',
-  imports: [CommonModule],
+  imports: [CommonModule, Card, Button],
   templateUrl: './blog-list.component.html',
   styleUrl: './blog-list.component.scss',
 })
